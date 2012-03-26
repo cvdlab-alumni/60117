0cd7967a7ba94563fea31b525828c5479ad257b8
Vittorio Cecchetto <v.cecchetto@gmail.com>
2012-03-26 16:52:21 +0200

    domain5

--- 20120326/domini.js
+++ 20120326/domini.js
@@ -5,4 +5,18 @@ DRAW(domain2)
 var domain3 = DOMAIN([[1,5],[1,3],[0,1]])([4,2,1])
 DRAW(domai3)
 
+var domain4 = DOMAIN([[0,10]])([10]);
+var mapping = function (p){
+	var u = p[0];
+	return [u,1];
+}
+var mapped = MAP(mapping)(domain4);
+DRAW(domain4)
 
+var domain5 = DOMAIN([[0,10]])([10]);
+var mapping = function (p){
+	var u = p[0];
+	return [u,u];
+}
+var mapped = MAP(mapping)(domain5);
+DRAW(domain5);
