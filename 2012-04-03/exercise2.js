3f65a0e5ac26e6683ac6b6574c30ecd0d458d4d1
Vittorio Cecchetto <v.cecchetto@gmail.com>
2012-04-04 00:10:52 +0200

    pilastri croce

--- 2012-04-03/exercise2.js
+++ 2012-04-03/exercise2.js
@@ -24,10 +24,36 @@ var pavimento = STRUCT([
 	T([0,1])([51,5])(SIMPLEX_GRID([[1],[1],[1]])),
 	T([0,1])([1,10])(SIMPLEX_GRID([[8],[12],[1]])),
 	T([0,1])([9,10])(SIMPLEX_GRID([[30],[7],[1]]))
-	]);
+]);
 
 //DRAW(S([0,1,2])([.1,.1,.1])(pavimento));
-DRAW(pavimento);
+//DRAW(pavimento);
+
+var pilastro_croce = STRUCT([
+	T([0,1])([-0.1,-0.02])(SIMPLEX_GRID([[0.08],[0.04],[3]])),
+	T([0,1])([-0.02,-0.1])(SIMPLEX_GRID([[0.04],[0.2],[3]])),
+	T([0,1])([0.02,-0.02])(SIMPLEX_GRID([[0.08],[0.04],[3]]))
+]);
+
+//DRAW(pilastro_croce);
+
+var pilastri = STRUCT([
+	T([0,1,2])([26,7,1])(pilastro_croce),
+	T([0,1,2])([26,14,1])(pilastro_croce),
+	T([0,1,2])([26 + 19 / 3, 7,1])(pilastro_croce),
+	T([0,1,2])([26 + 19 / 3, 14,1])(pilastro_croce),
+	T([0,1,2])([26 + 2 * 19 / 3, 7,1])(pilastro_croce),
+	T([0,1,2])([26 + 2 * 19 / 3, 14,1])(pilastro_croce),
+	T([0,1,2])([45,7,1])(pilastro_croce),
+	T([0,1,2])([45,14,1])(pilastro_croce)
+]);
+
+var pavilion = STRUCT([
+	pavimento,
+	pilastri
+]);
+
+DRAW(pavilion);
 
 }
 
