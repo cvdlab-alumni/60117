2a8f1658947f7066751c6caed7f0da4fcb1d4645
Vittorio Cecchetto <v.cecchetto@gmail.com>
2012-04-16 16:40:46 +0200

    organizzazione

---
+++ 2012-03-26.js
@@ -0,0 +1,68 @@
+var domain = DOMAIN([[1,5]])([4])
+DRAW(domain)
+var domain2 = DOMAIN([[1,5],[1,3]])([4,2])
+DRAW(domain2)
+var domain3 = DOMAIN([[1,5],[1,3],[0,1]])([4,2,1])
+DRAW(domai3)
+
+var domain4 = DOMAIN([[0,10]])([10]);
+var mapping = function (p){
+	var u = p[0];
+	return [u,1];
+}
+var mapped = MAP(mapping)(domain4);
+DRAW(mapped)
+
+var domain5 = DOMAIN([[0,10]])([10]);
+var mapping = function (p){
+	var u = p[0];
+	return [u,u];
+}
+var mapped = MAP(mapping)(domain5);
+DRAW(mapped);
+
+var domain6 = DOMAIN([[0,2*PI]])([120]);
+var mapping = function (p){
+	var u = p[0];
+	return [u,SIN(u)];
+}
+var mapped = MAP(mapping)(domain6);
+COLOR([1,0,0])(mapped)
+DRAW(mapped);
+
+var drawCircle = function (r,n){
+	var domain = DOMAIN([[0,2*PI]])([n]);
+	var mapping = function (p){
+		var u = p[0];
+		return [r*COS(u),r*SIN(u)];
+	}
+	var mapped = MAP(mapping)(domain);
+	COLOR([1,0,0])(mapped)
+	DRAW(mapped);
+}
+
+var drawCylinder = function (r,h,n,m,color){
+	var domain = DOMAIN([[0,2*PI],[0,h]])([n,m]);
+	var mapping = function (p){
+		var u = p[0];
+		var v = p[1];
+		return [r*COS(u),r*SIN(u),v];
+	}
+
+	var mapped = MAP(mapping)(domain);
+	COLOR(color)(mapped)
+	DRAW(mapped);
+}
+
+var drawSphere = function (r,n,m,color){
+	var domain = DOMAIN([[0,2*PI],[0,2*PI]])([n,m]);
+	var mapping = function (p){
+		var u = p[0];
+		var v = p[1];
+		return [r*COS(u)*SIN(v),r*SIN(u)*SIN(v),r*COS(v)];
+	}
+
+	var mapped = MAP(mapping)(domain);
+	COLOR(color)(mapped)
+	DRAW(mapped);
+}
