393b61000e07df7afaa731518248c13b52cea998
Vittorio Cecchetto <v.cecchetto@gmail.com>
2012-05-05 18:04:13 +0200

    stabilizzatore

--- 2012-05-04/exercise3.js
+++ 2012-05-04/exercise3.js
@@ -26,106 +26,85 @@ var grigliaxz_ruotata = R([1,2])(PI/2)(grigliaxz);
 var grigliayz = COLOR([1,0,0,.5])(griglia(-20,-8,10,6,30,14));
 var grigliayz_ruotata = R([0,2])(-PI/2)(grigliayz);
 
-// Stabilizzatori
+// Stabilizzatore orizzontale
 
-var dom1D = INTERVALS(1)(30);
-var dom2D = DOMAIN([[0,1],[0,1]])([15,30]);
+var stab_dom1D = INTERVALS(1)(60);
+var stab_dom2D = DOMAIN([[0,1],[0,1]])([60,60]);
 
-var c0 = [[0,0.6,0],[6,2.1,0],[12,3.5,0],[18,4.2,0],[24,4.8,0],[30,4.2,0],[34,3,0],[35,2.6,0],[36,2,0],[37,1.5,0],[38,1,0],[37,1.5,0],[36,2,0],[34,2.4,0],[30,3,0],[24,3,0],[18,3,0],[12,2.5,0],[6,1.5,0],[0,0.6,0]];
-var d1 = c0.map(function (p) {return [p[0], p[1], p[2]-6*36/7.5]});
-var s1 = c0.map(function (p) {return [p[0], p[1], p[2]+6*36/7.5]});
-var d2 = c0.map(function (p) {return [p[0], p[1]+0.5*36/7.5, p[2]-12*36/7.5]});
-var s2 = c0.map(function (p) {return [p[0], p[1]+0.5*36/7.5, p[2]+12*36/7.5]});
-var d3 = c0.map(function (p) {return [p[0], p[1]+1*36/7.5, p[2]-18*36/7.5]});
-var s3 = c0.map(function (p) {return [p[0], p[1]+1*36/7.5, p[2]+18*36/7.5]});
+var stab_or_cp = [[0,0,0],[0,1,6],[0,2,13],[0,1.5,18],[0,0,20],[0,-1.5,18],[0,0,13],[0,0,6],[0,0,0]];
 
-var curvesPoints = STRUCT(AA(POLYPOINT)([c0,d1,s1,d2,s2,d3,s3]));
-//DRAW(curvesPoints);
+var stab_or_cp_s7 = stab_or_cp.map(function (p) {return [p[0]+26, p[1]/2, p[2]/2+10]});
+var stab_or_cp_s6 = stab_or_cp.map(function (p) {return [p[0]+24, p[1], p[2]*17/20+3]});
+var stab_or_cp_s5 = stab_or_cp.map(function (p) {return [p[0]+22, p[1], p[2]]});
+var stab_or_cp_s4 = stab_or_cp.map(function (p) {return [p[0]+8, p[1], p[2]]});
+var stab_or_cp_s3 = stab_or_cp.map(function (p) {return [p[0]+6, p[1], p[2]*19.7/20+0.3]});
+var stab_or_cp_s2 = stab_or_cp.map(function (p) {return [p[0]+4, p[1], p[2]*18/20+2]});
+var stab_or_cp_s1 = stab_or_cp.map(function (p) {return [p[0]+2, p[1], p[2]*15/20+5]});
+var stab_or_cp_c0 = stab_or_cp.map(function (p) {return [p[0], p[1], p[2]*13/20+7]});
+var stab_or_cp_d1 = stab_or_cp.map(function (p) {return [p[0]-2, p[1], p[2]*15/20+5]});
+var stab_or_cp_d2 = stab_or_cp.map(function (p) {return [p[0]-4, p[1], p[2]*18/20+2]});
+var stab_or_cp_d3 = stab_or_cp.map(function (p) {return [p[0]-6, p[1], p[2]*19.7/20+0.3]});
+var stab_or_cp_d4 = stab_or_cp.map(function (p) {return [p[0]-8, p[1], p[2]]});
+var stab_or_cp_d5 = stab_or_cp.map(function (p) {return [p[0]-22, p[1], p[2]]});
+var stab_or_cp_d6 = stab_or_cp.map(function (p) {return [p[0]-24, p[1], p[2]*17/20+3]});
+var stab_or_cp_d7 = stab_or_cp.map(function (p) {return [p[0]-26, p[1]/2, p[2]/2+10]});
 
-var controls = AA(BEZIER(S0))([d3,d2,d1,c0,s1,s2,s3]);
+var stab_or_cc_s7 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s7);
+var stab_or_cc_s6 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s6);
+var stab_or_cc_s5 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s5);
+var stab_or_cc_s4 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s4);
+var stab_or_cc_s3 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s3);
+var stab_or_cc_s2 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s2);
+var stab_or_cc_s1 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s1);
+var stab_or_cc_c0 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_c0);
+var stab_or_cc_d1 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d1);
+var stab_or_cc_d2 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d2);
+var stab_or_cc_d3 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d3);
+var stab_or_cc_d4 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d4);
+var stab_or_cc_d5 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d5);
+var stab_or_cc_d6 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d6);
+var stab_or_cc_d7 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d7);
 
+DRAW(MAP(stab_or_cc_s7)(stab_dom1D));
+DRAW(MAP(stab_or_cc_s6)(stab_dom1D));
+DRAW(MAP(stab_or_cc_s5)(stab_dom1D));
+DRAW(MAP(stab_or_cc_s4)(stab_dom1D));
+DRAW(MAP(stab_or_cc_s3)(stab_dom1D));
+DRAW(MAP(stab_or_cc_s2)(stab_dom1D));
+DRAW(MAP(stab_or_cc_s1)(stab_dom1D));
+DRAW(MAP(stab_or_cc_c0)(stab_dom1D));
+DRAW(MAP(stab_or_cc_d1)(stab_dom1D));
+DRAW(MAP(stab_or_cc_d2)(stab_dom1D));
+DRAW(MAP(stab_or_cc_d3)(stab_dom1D));
+DRAW(MAP(stab_or_cc_d4)(stab_dom1D));
+DRAW(MAP(stab_or_cc_d5)(stab_dom1D));
+DRAW(MAP(stab_or_cc_d6)(stab_dom1D));
+DRAW(MAP(stab_or_cc_d7)(stab_dom1D));
 
-var curves = STRUCT(CONS(AA(MAP)(controls))(dom1D));
+var stab = NUBS(S1)(2)([0,0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,15,15])([
+	[28,0,17],
+	stab_or_cc_s7,
+	stab_or_cc_s6,
+	stab_or_cc_s5,
+	stab_or_cc_s4,
+	stab_or_cc_s3,
+	stab_or_cc_s2,
+	stab_or_cc_s1,
+	stab_or_cc_c0,
+	stab_or_cc_d1,
+	stab_or_cc_d2,
+	stab_or_cc_d3,
+	stab_or_cc_d4,
+	stab_or_cc_d5,
+	stab_or_cc_d6,
+	stab_or_cc_d7,
+	[-28,0,17]
+	]);
 
-//DRAW(curves);
-
-var wing = BEZIER(S1)(controls);
-var surf = MAP(wing)(dom2D);
-//DRAW(surf);
-
-var profilo_inscala = S([0,1,2])([7.5/36,7.5/36,7.5/36])(surf);
-var ala = R([0,2])(-PI/2)(profilo_inscala);
-var ali = STRUCT([T([1])([4]),ala,T([1,2])([-6,-2.5]),ala]);
-// DRAW(ali);
-
-
-// Profilo
-
-var dom1D = INTERVALS(1)(60);
-var dom2D = DOMAIN([[0,1],[0,1]])([60,60]);
-
-var controls1 = [[0,0,0],[0,0,20],[5,0,20],[-5,0,0]]; // p0, p1, t0, t1
-var c1 = CUBIC_HERMITE(S0)(controls1);
-var curve1 = MAP(c1)(dom1D);
-//DRAW(curve1);
-
-var controls11 = [[0,2,0],[0,2,20],[5,0,20],[-5,0,0]]; // p0, p1, t0, t1
-var c11 = CUBIC_HERMITE(S0)(controls11);
-var curve11 = MAP(c11)(dom1D);
-//DRAW(curve11);
-
-var controls2 = [[0,2,0],[0,5,0],[0,6,2],[0,10,15],[0,10,20],[0,2,20]];
-var knots2 = [0,0,0,1,2,3,4,4,4];
-var c2 = NUBS(S0)(2)(knots2)(controls2);
-var curve2 = MAP(c2)(dom1D);
-//DRAW(curve2);
-
-var controls33 = [[0,2,0],[0,2,20],[-5,0,20],[5,0,0]]; // p0, p1, t0, t1
-var c33 = CUBIC_HERMITE(S0)(controls33);
-var curve33 = MAP(c33)(dom1D);
-//DRAW(curve33);
-
-var controls3 = [[0,0,0],[0,0,20],[-5,0,20],[5,0,0]]; // p0, p1, t0, t1
-var c3 = CUBIC_HERMITE(S0)(controls3);
-var curve3 = MAP(c3)(dom1D);
-//DRAW(curve3);
-
-
-
-var curvesPoints = STRUCT(AA(POLYPOINT)([controls1,controls2,controls3]));
-//DRAW(curvesPoints);
-
-
-var s12h = CUBIC_HERMITE(S1)([c1,c2,[0,.01,0],[-0.1,0,0]]);
-var surface12h = MAP(s12h)(dom2D);
-//DRAW(surface12h);
-
-/*
-var s12 = BEZIER(S1)([c1,c11,c2,c33,c3]);
-var surf = MAP(s12)(dom2D);
-DRAW(surf);
-
-var s12 = BEZIER(S1)([c1,c11,c2,c3]);
-var surf = MAP(s12)(dom2D);
-DRAW(surf);
-
-
-var controls = [c1,c11,c2,c33,c3];
-var nubs = NUBS(S1)(3)([0,0,0,1,1,1,2,2,2])(controls);
-var model = MAP(nubs)(dom2D);
-DRAW(model);
-
-*/
-
-var c0 = BEZIER(S0)(c0);
-
-var curvesPoints = POLYPOINT([c0]);
-DRAW(curvesPoints);
-
+DRAW(MAP(stab)(stab_dom2D));
 
 // Disegno
 
 DRAW(grigliaxy);
 DRAW(grigliaxz_ruotata);
 DRAW(grigliayz_ruotata);
-
--- 2012-05-04/exercise4.js
+++ 2012-05-04/exercise4.js
@@ -46,6 +46,65 @@ var ali_ccmodel = STRUCT(CONS(AA(MAP)(ali_cc))(ali_dom1D));
 var ali_sup = BEZIER(S1)(ali_cc);
 var ali_model = MAP(ali_sup)(ali_dom2D);
 
+// Stabilizzatore orizzontale
+
+var stab_dom1D = INTERVALS(1)(60);
+var stab_dom2D = DOMAIN([[0,1],[0,1]])([60,60]);
+
+var stab_or_cp = [[0,0,0],[0,1,6],[0,2,13],[0,1.5,18],[0,0,20],[0,-1.5,18],[0,0,13],[0,0,6],[0,0,0]];
+
+var stab_or_cp_s7 = stab_or_cp.map(function (p) {return [p[0]+26, p[1]/2, p[2]/2+10]});
+var stab_or_cp_s6 = stab_or_cp.map(function (p) {return [p[0]+24, p[1], p[2]*17/20+3]});
+var stab_or_cp_s5 = stab_or_cp.map(function (p) {return [p[0]+22, p[1], p[2]]});
+var stab_or_cp_s4 = stab_or_cp.map(function (p) {return [p[0]+8, p[1], p[2]]});
+var stab_or_cp_s3 = stab_or_cp.map(function (p) {return [p[0]+6, p[1], p[2]*19.7/20+0.3]});
+var stab_or_cp_s2 = stab_or_cp.map(function (p) {return [p[0]+4, p[1], p[2]*18/20+2]});
+var stab_or_cp_s1 = stab_or_cp.map(function (p) {return [p[0]+2, p[1], p[2]*15/20+5]});
+var stab_or_cp_c0 = stab_or_cp.map(function (p) {return [p[0], p[1], p[2]*13/20+7]});
+var stab_or_cp_d1 = stab_or_cp.map(function (p) {return [p[0]-2, p[1], p[2]*15/20+5]});
+var stab_or_cp_d2 = stab_or_cp.map(function (p) {return [p[0]-4, p[1], p[2]*18/20+2]});
+var stab_or_cp_d3 = stab_or_cp.map(function (p) {return [p[0]-6, p[1], p[2]*19.7/20+0.3]});
+var stab_or_cp_d4 = stab_or_cp.map(function (p) {return [p[0]-8, p[1], p[2]]});
+var stab_or_cp_d5 = stab_or_cp.map(function (p) {return [p[0]-22, p[1], p[2]]});
+var stab_or_cp_d6 = stab_or_cp.map(function (p) {return [p[0]-24, p[1], p[2]*17/20+3]});
+var stab_or_cp_d7 = stab_or_cp.map(function (p) {return [p[0]-26, p[1]/2, p[2]/2+10]});
+
+var stab_or_cc_s7 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s7);
+var stab_or_cc_s6 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s6);
+var stab_or_cc_s5 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s5);
+var stab_or_cc_s4 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s4);
+var stab_or_cc_s3 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s3);
+var stab_or_cc_s2 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s2);
+var stab_or_cc_s1 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_s1);
+var stab_or_cc_c0 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_c0);
+var stab_or_cc_d1 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d1);
+var stab_or_cc_d2 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d2);
+var stab_or_cc_d3 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d3);
+var stab_or_cc_d4 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d4);
+var stab_or_cc_d5 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d5);
+var stab_or_cc_d6 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d6);
+var stab_or_cc_d7 = NUBS(S0)(2)([0,0,0,1,2,3,4,5,6,7,7,7])(stab_or_cp_d7);
+
+var stab = NUBS(S1)(2)([0,0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,15,15])([
+  [28,0,17],
+  stab_or_cc_s7,
+  stab_or_cc_s6,
+  stab_or_cc_s5,
+  stab_or_cc_s4,
+  stab_or_cc_s3,
+  stab_or_cc_s2,
+  stab_or_cc_s1,
+  stab_or_cc_c0,
+  stab_or_cc_d1,
+  stab_or_cc_d2,
+  stab_or_cc_d3,
+  stab_or_cc_d4,
+  stab_or_cc_d5,
+  stab_or_cc_d6,
+  stab_or_cc_d7,
+  [-28,0,17]
+  ]);
+
 // Disegno
 
 DRAW(grigliaxy);
@@ -57,4 +116,7 @@ var ali_model_S_R = R([0,2])(-PI/2)(S([0,1,2])([7.5/36,7.5/36,7.5/36])(ali_model
 var ali = STRUCT([T([1])([4]),ali_model_S_R,T([1,2])([-6,-2.5]),ali_model_S_R]);
 
 DRAW(ali);
+
+
+//DRAW(MAP(stab)(stab_dom2D));
 
